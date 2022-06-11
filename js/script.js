946286130d3f1a92e6ba3d2908281cdf41bfb6bf
Jhenia <36773626+Jhenia@users.noreply.github.com>
2022-06-11 17:41:53 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-        let header__burger = document.querySelector('.burger');
+ //Header burger     
+      let header__burger = document.querySelector('.burger');
         let header_menu = document.querySelector('.header__menu');
         let back = document.querySelector('body');
 
@@ -8,3 +9,18 @@
             header_menu.classList.toggle('active');
             back.classList.toggle('locked');
         }
+//Header burger   
+
+ // Submenu
+
+    let submenu = document.querySelector('.submenu');
+    let trigger = document.querySelector('.with__submenu');
+    let trigger__link = document.querySelector('.trigger__link');
+    let arrow = document.querySelector('.submenu__arrow');
+     trigger.onclick = function(){
+        submenu.classList.toggle('active');
+        arrow.classList.toggle('active');
+        trigger__link.classList.toggle('active');
+     }
+         
+     
